d67f3407ce2cb1ba13582f194946b1a5b21a33d1
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 23:20:48 +0300

    Added capacity validation for StorageProduct model

--- app/models/storage_product.rb
+++ app/models/storage_product.rb
@@ -4,4 +4,23 @@ class StorageProduct < ActiveRecord::Base
 
   validates :storage_id, uniqueness: { scope: :product_id,
                                  message: 'Each player should have only one storage record per product'}
+
+  validate :player_should_not_be_greedy
+
+  # before_validation :get_info_for_validation
+
+  private
+
+  # def get_info_for_validation
+  #   @player = Player.find(player_id)
+  #   @product = Product.find(product_id)
+  # end
+
+  def player_should_not_be_greedy
+    sum = StorageProduct.group(:storage_id).having(storage_id: storage_id).sum(:quantity)
+    if sum[storage_id] > ConfigProvider.instance.capacity
+      errors.add(:quantity, 'Player cannot overfill his storage')
+    end
+  end
+
 end
