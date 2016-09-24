37580dc7194ee126c913207e72f63a88121ea5cb
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 18:44:11 +0300

    Added title to a product and name to a player

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924141333) do
+ActiveRecord::Schema.define(version: 20160924154244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160924141333) do
     t.decimal  "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "products", force: :cascade do |t|
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160924141333) do
     t.integer  "availability"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "title"
   end
 
   create_table "storage_products", force: :cascade do |t|
