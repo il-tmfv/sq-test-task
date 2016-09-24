c4216aaf138e675ae95bea0d860f4e01ac09d27b
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 22:54:30 +0300

    Added validations for Offer model + updated create method

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,11 +8,17 @@ class OffersController < ApiController
   end
 
   def create
+    if params[:player_id].blank? || params[:product_id].blank? || params[:price].blank? || params[:quantity].blank?
+      return render status: 400, json: { error: ApiError::INVALID_ARGUMENTS }
+    end
+
     @offer = Offer.new(permitted_params)
-    if @offer.save
+
+    if @offer.valid?
+      @offer.save
       render_ok
     else
-      render status: 400, json: { error: ApiError::INVALID }
+      render status: 400, json: { error: @offer.errors.messages }
     end
   end
 
@@ -27,4 +33,5 @@ class OffersController < ApiController
   def permitted_params
     params.require(:offer).permit(:player_id, :product_id, :price, :quantity)
   end
+
 end
