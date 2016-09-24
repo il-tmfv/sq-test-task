5da505e0726507fa8fd501491e57ec2d3a34bdf7
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 17:03:33 +0300

    Added associations between players and products

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ActiveRecord::Base
+  has_many :products, through: :player_products
+  has_many :player_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  has_many :players, through: :player_products
+  has_many :player_products
 end
