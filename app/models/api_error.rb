de46a6a386e8f16fe87535dc14c0cbc8329ca300
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 22:49:51 +0300

    Added some extra API errors

--- app/models/api_error.rb
+++ app/models/api_error.rb
@@ -1,4 +1,19 @@
 class ApiError
+  INVALID_PRICE = {
+      code: 401,
+      message: 'Price is invalid'
+  }.freeze
+
+  NO_PRODUCT = {
+      code: 402,
+      message: 'Player does not have mentioned product'
+  }
+
+  NO_QUOTA = {
+      code: 403,
+      message: 'Player does not have enough free space at marketplace'
+  }.freeze
+
   NOT_FOUND = {
       code: 404,
       message: 'Record not found.'
