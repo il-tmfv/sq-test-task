2154d553837199fb57455e75f7d7a64958167b0e
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 22:49:31 +0300

    Added check for uniqueness of StorageProduct record

--- app/models/storage_product.rb
+++ app/models/storage_product.rb
@@ -1,4 +1,7 @@
 class StorageProduct < ActiveRecord::Base
   belongs_to :storage
   belongs_to :product
+
+  validates :storage_id, uniqueness: { scope: :product_id,
+                                 message: 'Each player should have only one storage record per product'}
 end
