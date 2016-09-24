97d8e42fe2458532dc583464cd49a87fc0b60d14
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-24 19:14:22 +0300

    Added random data generation to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+### products section
+
+Product.delete_all
+
+products = ConfigProvider.instance.products
+
+products.each_key { |k|
+  Product.create!(
+      title: k, 
+      price: products[k]['price'], 
+      required_level: products[k]['required_level']
+  )
+}
+
+### players & their storages section
+
+Player.delete_all
+
+players_count = 10
+storage_capacity = ConfigProvider.instance.capacity
+
+players_count.times { |i|
+  player = Player.create!(
+      name: "Player##{i}",
+      level: rand(0..10),
+      exp: rand(0.0..1.0).round(1),
+      balance: rand(10.0..110.0).round(1)
+  )
+  
+  player.storage = Storage.create!(capacity: storage_capacity)
+}
+
+### filling up users storages 
+
+Player.all.each { |player|
+  StorageProduct.create!(
+      storage_id: player.storage.id,
+      product_id: Product.all.sample.id,
+      quantity: rand(1...storage_capacity)
+  )
+}
