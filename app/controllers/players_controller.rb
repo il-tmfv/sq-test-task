b9e17735ac54f3328ce4831af4c40f632239852f
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-25 12:11:42 +0300

    Refactored show method for Players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,12 +8,6 @@ class PlayersController < ApiController
       return render status: 400, json: { error: ApiError::INVALID_ARGUMENTS }
     end
 
-    @player = Player.find_by_id(params[:id])
-
-    if @player.blank?
-      return render status: 404, json: { error: ApiError::NOT_FOUND }
-    end
-
-    @player
+    @player = Player.find(params[:id])
   end
 end
